b7cd8e3ecd063eaf79b4d0bb2dcd20b9d963db7f
wizacass <visak.pet0@gmail.com>
2021-02-10 15:46:43 +0200

    Import interaction scripts

---
+++ Examinable.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Examinable : MonoBehaviour
+{
+    public string textOnExamination;
+}
---
+++ InteractionController.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using TMPro;
+
+public class InteractionController : MonoBehaviour
+{
+    public TMP_Text DisplayText;
+    public float MaxInteractionDistance = 2f;
+    public float DisplayTextTime = 2f;
+
+    private float _timer;
+    private bool _isShowing;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            OnExamine();
+        }
+
+        TimerManagement();
+    }
+
+    private void OnExamine()
+    {
+        /*RaycastHit hitPoint;
+        if (Physics.Raycast(transform.position, transform.forward, out hitPoint, MaxInteractionDistance))
+        {
+            if (hitPoint.collider.GetComponent<Examinable>() != null)
+            {
+                Examinable examinable = hitPoint.collider.GetComponent<Examinable>();
+                DisplayText.text = examinable.textOnExamination;
+                timer = 0;
+                isShowing = true;
+            }
+        }*/
+
+        int mask = LayerMask.GetMask("Interactable");
+        if (Physics.Raycast(
+            transform.position,
+            transform.forward,
+            out var hit,
+            MaxInteractionDistance,
+            mask
+        ))
+        {
+            HandleInteraction(hit);
+        }
+    }
+
+    private void HandleInteraction(RaycastHit hit)
+    {
+        DisplayText.text = hit.transform.gameObject.name;
+        _timer = 0;
+        _isShowing = true;
+    }
+
+    private void TimerManagement()
+    {
+        if (_isShowing)
+        {
+            _timer += Time.deltaTime;
+            if (_timer >= DisplayTextTime)
+            {
+                _isShowing = false;
+            }
+        }
+        else
+        {
+            DisplayText.text = "";
+        }
+    }
+}
